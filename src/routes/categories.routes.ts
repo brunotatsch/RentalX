7b602e1d66666e16fae2836cfda82761313989ea
Bruno Tatsch <brunotatsch@gmail.com>
2021-09-01 20:31:42 -0300

    Finalizado Aula de iniciando api

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,30 @@
+import { request, response, Router } from "express";
+import { Category } from "../model/Category";
+import { CategoryRepository } from "../repositories/CategoriesRepository";
+
+const categoriesRoutes = Router();
+const categoryRepository = new CategoryRepository();
+
+
+categoriesRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+  const categoryAlreadyExists = categoryRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error : "Category already exists !!!"})
+  }
+  categoryRepository.create({ name, description });
+
+  return response.status(201).send();
+
+})
+
+categoriesRoutes.get("/", (request, response) => {
+
+  const all = categoryRepository.list();
+
+  return response.json(all);
+})
+
+export { categoriesRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,10 @@
 import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 const port: number = 3333;
 
 app.use(express.json());
-
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World !!!" })
-});
-
-app.post("/courses", (request, response) => {
-  const { name } = request.body;
-  return response.json({ name });
-})
+app.use("/categories", categoriesRoutes);
 
 app.listen(port, () => console.log(`Server is Running on port ${port}`))
